2fe407a0042612bbddcaee4df8fd9061eba5b085
SindileM <smakhathini24@gmail.com>
2022-01-21 11:49:24 +0200

    vue-modal

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+<h1>{{ title }}</h1>
+<p>Have a look at our promotion</p>
+<div v-if="showModal">
+ <Modal theme="promomtion" @close="toggleModal">
+ <template v-slot:links>
+   <a href="#">Register</a>
+   <a href="#">Find Out More</a>
+ </template>
+ <h1>Sindile Makhathini</h1>
+ <p>Cone get your 50% discount now whilst stock last!</p>
+ </Modal>
+</div>
+<button @click="toggleModel">Open Modal</button>
+</template>
+
+<script>
+import Modal from "./components/Modal.vue";
+export default {
+  name: 'App',
+  components: {
+     Modal 
+    },
+  data() {
+    return {
+      title: 'Sindile Makhathini',
+      header: 'Sign up to win a prize!',
+      text: 'Come get your 50% discount now whilst stock last!',
+      showModal: false
+    }
+  },
+  methods: {
+    toggelModal(){
+      this.showModal = !this.showModal;
+    }
+    }
+}
+</script>
+
